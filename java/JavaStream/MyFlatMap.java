320205f8a14d16faa8a600439ee02c6e380449c8
Guoyuan Xiao <guoyuan_xiao@apple.com>
2021-01-18 12:18:52 -0800

    Add reduce example using binary function

--- java/JavaStream/MyFlatMap.java
+++ java/JavaStream/MyFlatMap.java
@@ -18,21 +18,34 @@ public class MyFlatMap {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
 
-        // Create persons
-        IntStream.range(1, 4).forEach( i -> people.add(new Person("Person " + i)));
+        // Create persons of 3
+        IntStream.range(1, 4).forEach( i -> people.add(new Person("Person " + i, i)));
 
         //Create children
         people.forEach(p -> IntStream.range(1, 4).forEach(i -> p.children.add(new Child("Child " + i + "< -" + p.name))));
+
+        //Flat map would convert one stream to flat out all its elements to another bigger stream in this case
+        //Key word: one to many
         people.stream().flatMap(p -> p.children.stream()).forEach(c -> System.out.println(c.name));
+
+        // Reduce to find the oldest person using a binary function
+        // Key word: use binary function to reduce
+        people.stream().reduce((p1, p2) -> p1.age > p2.age ? p1 : p2).ifPresent(p -> System.out.println(p.name));
     }
 }
 
 class Person {
     String name;
+    int age;
     List<Child> children = new ArrayList<>();
 
     Person(String name) {
         this.name = name;
+    }
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
 
